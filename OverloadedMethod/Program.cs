38ae403c56e9dfa88163a3ad13e19c0ce04da022
Steven Hollidge <stevenhollidge@hotmail.com>
2013-05-23 22:13:55 +0200

    initial commit

---
+++ OverloadedMethod/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OverloadedMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var shapes = new List<Shape> {new Circle(), new Triangle()};
+
+            foreach (var shape in shapes)
+            {
+                // option 4:  so what about this....?  no dice!      
+                (shape as dynamic).Execute(); 
+            }
+        }
+    }
+
+    class Shape { }
+
+    class Circle : Shape 
+    {
+        public int Circumference { get { return 10; } }
+    }
+
+    class Triangle : Shape
+    {
+        public int HypotenuseLength { get { return 20; } }
+    }
+
+    static class ExtensionMethods
+    {
+        public static void Execute(this Circle circle)
+        {
+            Console.WriteLine("Executing with a Circle with circumference {0}!", circle.Circumference);
+        }
+
+        public static void Execute(this Triangle triangle)
+        {
+            Console.WriteLine("Executing with a Triangle hypotenuse length of {0}!", triangle.HypotenuseLength);
+        }
+    }
+}
